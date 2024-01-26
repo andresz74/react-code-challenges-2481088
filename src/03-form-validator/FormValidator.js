3a6b2a2645e8a322deedefd4766bba13a4e1e227
Andres Zenteno <andres.zenteno@pointsbet.com>
2024-01-25 19:19:57 -0500

    feat: update solution

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,29 +1,56 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const isEmail = email => {
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/gm;
+    return emailRegex.test(email);
+  };
+  const pwdLongEnough = pwd => pwd.length > 8;
+  const matchPasswords = (pwd1, pwd2) => pwd1 === pwd2;
+
+  const ErrorsMessage = () => {
+    return errors.map((error, index) => <p key={index}>{error}</p>);
+  };
+
+  const handleSubmit = e => {
+    const errorsMsg = [];
+    e.preventDefault();
+    if (!isEmail(email)) errorsMsg.push(`It's not an email.`);
+    if (!pwdLongEnough(password))
+      errorsMsg.push(`Password must be at least 8 characters`);
+    if (!matchPasswords(password, passwordConfirm))
+      errorsMsg.push(`Passwords don't match`);
+    setErrors(errorsMsg);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
-      <label htmlFor='email'>Email</label>
+      <label htmlFor="email">Email</label>
       <input
-        type='text' name='email'
+        type="text"
+        name="email"
         onChange={e => setEmail(e.target.value)}
       />
-      <label htmlFor='password'>Password</label>
+      <label htmlFor="password">Password</label>
       <input
-        type='password' name='password'
+        type="password"
+        name="password"
         onChange={e => setPassword(e.target.value)}
       />
-      <label htmlFor='password-confirm'>Confirm Password </label>
+      <label htmlFor="password-confirm">Confirm Password </label>
       <input
-        type='password' name='password-confirm'
+        type="password"
+        name="password-confirm"
         onChange={e => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <input type="submit" value="Submit" />
+      <ErrorsMessage />
     </form>
-  )
+  );
 }
