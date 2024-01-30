58a22f03585e626cdbc0dda89a12f23c70be1762
Andres Zenteno <azenteno74@gmail.com>
2024-01-30 18:44:32 -0500

    feat: update solution

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,23 @@
-export default function DogPics () {
+import { useState } from "react";
+
+export default function DogPics() {
   // API: https://dog.ceo/dog-api/
+  const [dogImgSrc, setDogImgSrc] = useState(
+    "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg"
+  );
+  const getDogImage = async () => {
+    try {
+      const result = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await result.json()
+      setDogImgSrc(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={dogImgSrc} alt="Dog" />
+      <button onClick={(e) => getDogImage()}>🐶</button>
     </div>
-  )
+  );
 }
