31f719948cc884982f0a84f1ac2663d50814a8e0
Andres Zenteno <azenteno74@gmail.com>
2024-01-30 19:58:58 -0500

    feat: add solution

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 export default function WindowEvent () {
+  useEffect(()=>{
+    const activeAlert = () => alert('Hey there!');
+    window.addEventListener('dblclick', activeAlert);
+
+    return ()=> window.removeEventListener('dblclick', activeAlert);
+    
+  },[])
   return (
     <h2>Window event active</h2>
   )
