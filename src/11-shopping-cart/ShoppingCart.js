50cbe88d0e16f6afc7a16d4af60fb969a7c405fd
Andres Zenteno <azenteno74@gmail.com>
2024-02-17 00:00:50 -0500

    feat: restore file

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -16,53 +16,7 @@ const items = [
 ];
 
 function ShoppingCart() {
-  // const cart = [{ name: "apple", quantity: 3, price: 0.39 }];
-  const [cart, setCart] = useState([]);
-
-  const findItemInCart = (item) => {
-    return cart.find((i) => item.name === i.name);
-  };
-
-  const addCart = (item) => {
-    const itemInCart = findItemInCart(item);
-    if (itemInCart) {
-      itemInCart.quantity++;
-      setCart([...cart]);
-    } else {
-      setCart((prevCart) => [...prevCart, { ...item, quantity: 1 }]);
-    }
-  };
-
-  const increase = (item) => {
-    const itemInCart = findItemInCart(item);
-    itemInCart.quantity++;
-    setCart([...cart]);
-  };
-
-  // Using 'filter' will create a new array.
-  // const decrease = (item) => {
-  //   const itemInCart = findItemInCart(item);
-  //   if (itemInCart.quantity > 1) {
-  //     itemInCart.quantity--;
-  //     setCart([...cart]);
-  //   } else {
-  //     setCart(prevCart => prevCart.filter(i => i.name !== itemInCart.name));
-  //   }
-  // };
-
-  const decrease = (item) => {
-    // get the index of the item in the cart
-    const itemIndex = cart.findIndex((i) => i.name === item.name);
-    if (itemIndex > -1) {
-      const item = cart[itemIndex];
-      if (item.quantity > 1) {
-        item.quantity--;
-      } else {
-        cart.splice(itemIndex, 1);
-      }
-      setCart([...cart]);
-    }
-  };
+  const cart = [{ name: "apple", quantity: 3, price: 0.39 }];
 
   return (
     <div>
@@ -74,7 +28,7 @@ function ShoppingCart() {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button onClick={() => addCart(item)}>Add to Cart</button>
+              <button>Add to Cart</button>
             </div>
           ))}
         </div>
@@ -84,20 +38,17 @@ function ShoppingCart() {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button onClick={() => decrease(item)}>-</button>
+                <button>-</button>
                 {item.quantity}
-                <button onClick={() => increase(item)}>+</button>
+                <button>+</button>
               </p>
-              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
+              <p>Subtotal: ${item.quantity * item.price}</p>
             </div>
           ))}
         </div>
       </div>
       <div className="total">
-        <h2>
-          Total: $
-          {cart.reduce((totalCart, item) => totalCart + item.quantity * item.price, 0).toFixed(2)}
-        </h2>
+        <h2>Total: $0.00</h2>
       </div>
     </div>
   );
